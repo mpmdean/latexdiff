dea4ceffc5b87e180f5d4c713c8f276fd47d393e
Mark Dean <mpmdean@gmail.com>
2022-11-06 00:12:14 +0900

    fix bibtex error and add exaplanation

--- latexdiff_project.sh
+++ latexdiff_project.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Make difference files for main.tex and supp.tex.
+# Usage latexdiff_project old_folder new_folder
+# which will create pdfs in the root folder.
+# Folder names cannot contain spaces.
 
 latexdiff $1main.tex $2main.tex > $2main_diffs.tex
 latexdiff $1supp.tex $2supp.tex > $2supp_diffs.tex
@@ -9,7 +13,7 @@ pdflatex main_diffs.tex
 pdflatex main_diffs.tex
 
 pdflatex supp_diffs.tex
-bibtex supp_diffs.tex
+bibtex supp_diffs
 pdflatex supp_diffs.tex
 pdflatex supp_diffs.tex
 
